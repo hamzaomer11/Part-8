b16fe45d1e8d579cb77e25b7f9808093e626cab8
hamesomer11 <hamzaomer45@gmail.com>
2024-10-27 18:02:30 +0000

    All books: Exercise 8.2

--- index.js
+++ index.js
@@ -80,16 +80,26 @@ let books = [
 ]
 
 const typeDefs = `
+
+  type Book { 
+    title: String!
+    author: String!
+    published: String!
+    genres: [String!]
+  }
+
   type Query {
     authorCount: Int!
     bookCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
       authorCount: () => authors.length,
-      bookCount: () => books.length
+      bookCount: () => books.length,
+      allBooks: () => books
   }
 }
 
