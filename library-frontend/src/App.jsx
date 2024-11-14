b5ed59160cc8b6e7320daf4d6f2929705ec477c6
hamesomer11 <hamzaomer45@gmail.com>
2024-11-14 08:58:55 +0000

    Login: Exercise 8.18

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,20 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 
 import {
   BrowserRouter as Router,
   Routes, Route, Link
 } from 'react-router-dom'
 
+import { useApolloClient } from "@apollo/client";
+
 const App = () => {
 
   const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
 
   const padding = {
     padding: 5
   }
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('library-user-token')
+    if (loggedUserJSON) {
+      setToken(loggedUserJSON)
+    }
+  }, [])
 
   const notify = (message) => {
     setErrorMessage(message)
@@ -23,15 +35,30 @@ const App = () => {
     }, 10000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    )
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
-
       <Router>
         <div>
           <Link style={padding} to="/">authors</Link>
           <Link style={padding} to="/books">books</Link>
           <Link style={padding} to="/add-book">add</Link>
+          <Link style={padding} onClick={logout}>logout</Link>
         </div>
         <Routes>
           <Route path="/" element={<Authors setErrorMessage={setErrorMessage}/>} />
