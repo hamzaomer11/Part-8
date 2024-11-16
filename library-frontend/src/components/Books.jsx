6357dd923f2e329c192b10cf5b61263ab90b48f1
hamesomer11 <hamzaomer45@gmail.com>
2024-11-16 20:15:33 +0000

    Books by genre: Exercise 8.19 - commit #1

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react"
 import { useQuery } from "@apollo/client"
 import { ALL_BOOKS } from "../../queries"
 
 const Books = () => {
+  const [selectedGenre, setSelectedGenre] = useState("All")
 
   const result = useQuery(ALL_BOOKS)
   console.log(result)
@@ -10,25 +12,49 @@ const Books = () => {
     return <div>loading...</div>
   }
 
+  const filteredBooks = selectedGenre === "All"
+    ? result.data.allBooks
+    : result.data.allBooks.filter(books => 
+      books.genres.includes(selectedGenre));
+
   return (
     <div>
       <h2>books</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {result.data.allBooks.map((b) => (
-            <tr key={b.title}>
-              <td>{b.title}</td>
-              <td>{b.author.name}</td>
-              <td>{b.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        {filteredBooks.length > 0 && (
+          <div>
+            <p>in genre <strong>{selectedGenre}</strong></p>
+            <table>
+              <tbody>
+                <tr>
+                  <th></th>
+                  <th>author</th>
+                  <th>published</th>
+                </tr>
+                {filteredBooks.map((b) => (
+                  <tr key={b.title}>
+                    <td>{b.title}</td>
+                    <td>{b.author.name}</td>
+                    <td>{b.published}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+      <div>
+        {result.data.allBooks.map(books => (
+          books.genres.map((genre) => (
+            <button
+            key={genre}
+            onClick={() => setSelectedGenre(genre)}
+          >
+            {genre}
+          </button>
+          ))
+        ))}
+      </div>
     </div>
   )
 }
