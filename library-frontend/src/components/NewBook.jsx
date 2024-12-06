0e0c93ceb3a47019b89df83db0be7ddf6ce12007
hamesomer11 <hamzaomer45@gmail.com>
2024-12-06 15:27:52 +0000

    Exercise 8.25: Subscriptions - client - part-2 - commit #1

--- library-frontend/src/components/NewBook.jsx
+++ library-frontend/src/components/NewBook.jsx
@@ -2,6 +2,22 @@ import { useState } from 'react'
 import { useMutation, useSubscription } from "@apollo/client"
 import { NEW_BOOK, ALL_BOOKS, ALL_AUTHORS, BOOK_ADDED } from '../../queries'
 
+export const updateCache = (cache, query, addedBook) => {
+  const uniqByTitle = (a) => {
+    let seen = new Set()
+    return a.filter((item) => {
+      let k = item.name
+      return seen.has(k) ? false : seen.add(k)
+    })
+  }
+
+  cache.updateQuery(query, ({ allBooks }) => {
+    return {
+      allBooks: uniqByTitle(allBooks.concat(addedBook)),
+    }
+  })
+}
+
 const NewBook = ({ setError }) => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
@@ -10,17 +26,21 @@ const NewBook = ({ setError }) => {
   const [genres, setGenres] = useState([])
 
   const [ createBook ] = useMutation(NEW_BOOK, {
-    refetchQueries: [ {query: ALL_BOOKS, variables: {genre: ""}}, {query: ALL_AUTHORS} ],
+    /* refetchQueries: [ {query: ALL_BOOKS, variables: {genre: ""}}, {query: ALL_AUTHORS} ], */
     onError: (error) => {
       const messages = error.graphQLErrors.map(e => e.message).join('\n')
       setError(messages)
+    },
+    update: (cache, response) => {
+      updateCache(cache, { query: ALL_BOOKS, variables: {genre: ""} }, response.data.addBook)
     }
   })
 
   useSubscription(BOOK_ADDED, {
-    onData: ({ data }) => {
+    onData: ({ data, client }) => {
       const addedBook = data.data.bookAdded
-      window.alert(`${addedBook.title} added`)
+      window.alert(`The book "${addedBook.title}" by ${addedBook.author.name} has been added!`)
+      updateCache(client.cache, { query: ALL_BOOKS, variables: {genre: ""} }, addedBook)
     }
   })
 
